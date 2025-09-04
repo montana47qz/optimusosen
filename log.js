728b38c1590aaaea9bf6a328fc1f7231ccef4817
montana47qz <nurjanes@ya.ru>
2025-09-05 01:18:37 +0500

    log.js

--- log.js
+++ log.js
@@ -1,25 +1,27 @@
 export default async function handler(req, res) {
-  const BOT_TOKEN = "8099430341:AAEzfX7LCh5xFbNHAo1CeGA-L1fEEFTDVPA";
-  const CHAT_ID = "-1002729269078"; // подставь свой реальный ID группы или свой личный
+  const BOT_TOKEN = process.env.BOT_TOKEN;
+  const CHAT_ID = process.env.CHAT_ID;
 
-  const testMessage = "🔥 Проверка связи с ботом!";
+  if (!BOT_TOKEN || !CHAT_ID) {
+    return res.status(500).json({ error: "BOT_TOKEN или CHAT_ID не заданы" });
+  }
 
   try {
+    const { message } = req.body || {};
+
     const tgRes = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         chat_id: CHAT_ID,
-        text: testMessage
-      })
+        text: message || "Тестовое сообщение от Vercel"
+      }),
     });
 
     const data = await tgRes.json();
-    console.log("Ответ Telegram:", data);
-
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (err) {
-    console.error("Ошибка:", err);
-    res.status(500).json({ error: "Ошибка при отправке" });
+    console.error("Ошибка при запросе в Telegram:", err);
+    return res.status(500).json({ error: "Не удалось отправить" });
   }
 }
