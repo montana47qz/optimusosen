1d1359afaa5fcf2ec6d1d5d1fd279d58351afc18
montana47qz <nurjanes@ya.ru>
2025-09-05 01:10:49 +0500

    log.js

--- log.js
+++ log.js
@@ -2,18 +2,23 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { message } = req.body;
 
+    const BOT_TOKEN = "8099430341:AAEzfX7LCh5xFbNHAo1CeGA-L1fEEFTDVPA"; // твой токен
+    const CHAT_ID = "-1002729269078"; // твой chat_id
+
     try {
-      // Отправка в Telegram
-      await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+      const tgRes = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          chat_id: process.env.CHAT_ID,
+          chat_id: CHAT_ID,
           text: message || "пустое сообщение"
         })
       });
 
-      res.status(200).json({ ok: true });
+      const data = await tgRes.json();
+      console.log("Ответ Telegram:", data);
+
+      res.status(200).json({ ok: true, telegram: data });
     } catch (err) {
       console.error("Ошибка при отправке в Telegram:", err);
       res.status(500).json({ error: "Ошибка телеграма" });
