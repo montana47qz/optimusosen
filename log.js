15263d8988cf3aae6540a10ccc8c866138ef87fe
montana47qz <nurjanes@ya.ru>
2025-09-05 00:19:17 +0500

    log.js

--- log.js
+++ log.js
@@ -1,24 +1,24 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { message } = req.body;
 
-    const BOT_TOKEN = process.env.BOT_TOKEN;
-    const CHAT_ID = process.env.CHAT_ID;
-
     try {
-      await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      // Отправка в Telegram
+      await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ chat_id: CHAT_ID, text: message }),
+        body: JSON.stringify({
+          chat_id: process.env.CHAT_ID,
+          text: message || "пустое сообщение"
+        })
       });
-      res.status(200).json({ status: "ok" });
+
+      res.status(200).json({ ok: true });
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ status: "error", error: err.toString() });
+      console.error("Ошибка при отправке в Telegram:", err);
+      res.status(500).json({ error: "Ошибка телеграма" });
     }
   } else {
-    res.status(405).json({ status: "Method Not Allowed" });
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
